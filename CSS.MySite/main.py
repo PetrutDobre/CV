776274f448a48f6c93f11a4081676796717983e6
PetrutDobre <dobre.petrut@yahoo.com>
2023-07-15 20:51:38 +0300

    Added CSS File

---
+++ CSS.MySite/main.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+with open("CSS.MYSITE") as file:
+   contents= file.read()
+
+def prime_checker(number):
+    is_prime == True
+    for i in range(2, number)
+        if number % i == 0:
+            is_prime == False
+    if is_prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number")
+
